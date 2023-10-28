1e88059ab19caa2289068ea6b4c375485a42449e
Tricia Lybrook <tlybrook@umich.edu>
2023-10-27 17:05:02 -0700

    new changes to image preprocessing

--- logistic_regression.py
+++ logistic_regression.py
@@ -10,37 +10,54 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import random
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import recall_score
+
 #from data_read import img_preprocess
 
 root_folder = "./chest_scan_data/"
 outcome_folders = ["/large.cell.carcinoma", "/normal", "/squamous.cell.carcinoma", "/adenocarcinoma"]
-
+split_folders = ["train", "test", "valid"]
 #%%
-def img_preprocess(split_folder, root_folder=root_folder, outcome_folders=outcome_folders):
+def img_preprocess(split_folders=split_folders, root_folder=root_folder, outcome_folders=outcome_folders):
     imgs = []
     values = []
-    for i in outcome_folders:
-        folder_concat = root_folder + str(split_folder) + i
-        files = os.listdir(folder_concat)
-        for file in files:
-            im = Image.open(f"{folder_concat}/{file}")
-            new_image = im.resize((400, 300))
-            image_array  = np.asarray(new_image.convert('RGB')).astype('float32')
-            imgs.append(image_array)
+    del_count = 0
+    for i in split_folders:
+        for j in outcome_folders:
+            folder_concat = root_folder + i + j
+            files = os.listdir(folder_concat)
+            for file in files:
+                im = Image.open(f"{folder_concat}/{file}")
+                new_image = im.resize((400, 300))
+                image_array  = np.asarray(new_image.convert('RGB')).astype('float32')
+                if len(imgs) != 0 and np.any(np.all(image_array == imgs, axis=1)):
+                    del_count += 1
+                else:
+                    imgs.append(image_array)
 
-            if i != "/normal":
-                values.append(1)
-            else:
-                values.append(0)
+                    if j != "/normal":
+                        values.append(1)
+                    else:
+                        values.append(0)
+
     zipped = list(zip(imgs, values))
     random.shuffle(zipped)
     imgs, values = zip(*zipped)
+    # Notes for MATT below:
+    # maybe train test split the data here? or do it in its own function with the 
+    #reshaping and casting to as array that we do below
 
     return imgs, values
 
 input_size = 400*300*3 #these are the dimensions of the resized images
 
 #%%
+images, values = img_preprocess()
+
+#%%
+#Notes for MATT Below
+# this section needs to be reworked to split the data and the function calls are wrong
 X_train, y_train = img_preprocess("train") 
 X_train = np.array(X_train)
 X_train = X_train.reshape(X_train.shape[0], -1) / 255
@@ -56,23 +73,13 @@ X_val = np.array(X_val)
 X_val = X_val.reshape(X_val.shape[0], -1) / 255
 y_val = np.array(y_val)
 
-#should try to shuffle the data
 #%%
-num_classes = 2
 
-model = Sequential() 
-model.add(Dense(1, input_dim=input_size, activation='sigmoid')) 
-
-# Hyperparameters
-batch_size = 25
-learning_rate = 1e-5
-nb_epoch = 5
-model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=["accuracy", "Recall"]) 
-history = model.fit(X_train, y_train, validation_data=(X_val, y_val)) 
-score = model.evaluate(X_test, y_test, verbose=0) 
-print('Test score:', score[0]) 
-print('Test accuracy:', score[1])
-print('Test recall:', score[2])
+clf = LogisticRegression(random_state=0, max_iter=500).fit(X_train, y_train)
+preds = clf.predict(X_test)
+probs = clf.predict_proba(X_test)
+score = clf.score(X_test, y_test)
+recall = recall_score(y_test, preds)
 
 
-# %%
+#%%
