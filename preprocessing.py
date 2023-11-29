7ad3bedfa333092e66730ff69faffd33f0ba4e3b
Tricia Lybrook <tlybrook@umich.edu>
2023-11-29 14:35:22 -0800

    minor changes to keras preprocess file

--- preprocessing.py
+++ preprocessing.py
@@ -28,7 +28,7 @@ def split_data(data_dir, batch_size = 32):
         label_mode='categorical',
         color_mode="grayscale",
         subset="validation",
-        shuffle=False,
+        shuffle=True,
         seed=42,
         batch_size=batch_size,
         image_size=image_resize)
@@ -42,12 +42,10 @@ def split_data(data_dir, batch_size = 32):
     
     return train_ds, val_ds, test_ds
 
-
 def normalize_data(ds):
     normalization_layer = tf.keras.layers.Rescaling(1./255)
     normalized_ds = ds.map(lambda x, y: (normalization_layer(x), y))
     return normalized_ds
-
 
 def data_augmentation(train_ds, rotation_val = 0.5, flip_orientation = None):
     flip_layer = tf.keras.layers.RandomFlip(flip_orientation, input_shape=(256, 256, 1), seed=42)
