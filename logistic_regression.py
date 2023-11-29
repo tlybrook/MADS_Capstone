c13f5155b2b9ce48633b896ee3c1d06a00f44300
Tricia Lybrook <tlybrook@umich.edu>
2023-11-28 18:40:18 -0800

    updates to multiclass log reg and file clean up

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,85 +1,79 @@
 #%%
-from keras.models import Sequential 
-from keras.layers import Dense, Activation 
-from PIL import Image
+from PIL import Image, ImageOps
 import os
-import imagesize
 import pandas as pd
 import matplotlib.pyplot  as plt
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-import random
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import recall_score
+from sklearn.metrics import recall_score, accuracy_score
+from sklearn.model_selection import train_test_split
+import random
 
-#from data_read import img_preprocess
-
-root_folder = "./chest_scan_data/"
-outcome_folders = ["/large.cell.carcinoma", "/normal", "/squamous.cell.carcinoma", "/adenocarcinoma"]
-split_folders = ["train", "test", "valid"]
+root_folder = "./final_dataset"
 #%%
-def img_preprocess(split_folders=split_folders, root_folder=root_folder, outcome_folders=outcome_folders):
+# This function preprocesses the data to prepare for logistic regression train/test/valid split.
+# Here we resize image, convert to grayscale, convert to array, and normalize array values to be between 0 and 1. 
+def log_reg_preprocess(root_folder=root_folder):
     imgs = []
-    values = []
-    del_count = 0
-    for i in split_folders:
-        for j in outcome_folders:
-            folder_concat = root_folder + i + j
-            files = os.listdir(folder_concat)
-            for file in files:
-                im = Image.open(f"{folder_concat}/{file}")
-                new_image = im.resize((400, 300))
-                image_array  = np.asarray(new_image.convert('RGB')).astype('float32')
-                if len(imgs) != 0 and np.any(np.all(image_array == imgs, axis=1)):
-                    del_count += 1
-                else:
-                    imgs.append(image_array)
+    labels = []
+    image_size = (256, 256)
+    dict_keys = {'adenocarcinoma': 0, 'large.cell.carcinoma': 1, 'normal': 2, 'squamous.cell.carcinoma': 3}
 
-                    if j != "/normal":
-                        values.append(1)
-                    else:
-                        values.append(0)
+    folders = sorted(os.listdir(f"{root_folder}"))
+    for i in folders:
+        files = os.listdir(f"{root_folder}/{i}")
+        label_key = dict_keys[i]
 
+        for file in files:
+            im = Image.open(f"{root_folder}/{i}/{file}")
+            new_image = im.resize(image_size)
+            gray_image = ImageOps.grayscale(new_image) 
+            image_array  = np.asarray(gray_image).astype('float32')
+            normalized_arr = image_array / 255
+            imgs.append(normalized_arr)
+            labels.append(label_key)
+
+    return imgs, labels
+
+# Here we are splitting the data into train/test 70/30
+def split_data(imgs, values):
     zipped = list(zip(imgs, values))
     random.shuffle(zipped)
     imgs, values = zip(*zipped)
-    # Notes for MATT below:
-    # maybe train test split the data here? or do it in its own function with the 
-    #reshaping and casting to as array that we do below
+    
+    # In the first step we will split the data in training and remaining dataset
+    X_train, X_test, y_train, y_test = train_test_split(imgs, values, train_size=0.7, random_state=42)
 
-    return imgs, values
+    X_list = [X_train, X_test] 
+    y_list = [y_train, y_test]
+    final_list = []
 
-input_size = 400*300*3 #these are the dimensions of the resized images
+    for i in X_list:
+        temp = np.array(i)
+        temp_reshape = temp.reshape(temp.shape[0], -1)
+        final_list.append(temp_reshape)
+    for j in y_list:
+        temp = np.array(j)
+        final_list.append(temp)
 
-#%%
-images, values = img_preprocess()
+    return final_list
 
-#%%
-#Notes for MATT Below
-# this section needs to be reworked to split the data and the function calls are wrong
-X_train, y_train = img_preprocess("train") 
-X_train = np.array(X_train)
-X_train = X_train.reshape(X_train.shape[0], -1) / 255
-y_train = np.array(y_train)
+def logistic_reg(split_list):
+    #X_train, X_test, y_train, y_test
+    clf = LogisticRegression(random_state=42, max_iter=500).fit(split_list[0], split_list[2])
+    y_preds = clf.predict(split_list[1])
+    acc_score = accuracy_score(split_list[3], y_preds)
+    recall = recall_score(split_list[3], y_preds, average="macro")
 
-X_test, y_test = img_preprocess("test")
-X_test = np.array(X_test)
-X_test = X_test.reshape(X_test.shape[0], -1) / 255
-y_test = np.array(y_test)
-
-X_val, y_val = img_preprocess("valid")
-X_val = np.array(X_val)
-X_val = X_val.reshape(X_val.shape[0], -1) / 255
-y_val = np.array(y_val)
+    return (acc_score, recall)
 
 #%%
+imgs, values = log_reg_preprocess()
+split_list = split_data(imgs, values)
+print(logistic_reg(split_list))
 
-clf = LogisticRegression(random_state=0, max_iter=500).fit(X_train, y_train)
-preds = clf.predict(X_test)
-probs = clf.predict_proba(X_test)
-score = clf.score(X_test, y_test)
-recall = recall_score(y_test, preds)
-
-
-#%%
+# %%
+if __name__ == '__main__':
+    imgs, values = log_reg_preprocess()
+    split_list = split_data(imgs, values)
+    print(logistic_reg(split_list))
